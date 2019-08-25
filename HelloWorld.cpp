44e0bec2fed6203211319781f98b3ac4d3597a09
lokesh-stack <tammineni.lokesh@gmail.com>
2019-08-25 11:13:17 +0530

    learning git

---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+////============================================================================
+//// Name        : HelloWorld.cpp
+//// Author      : Lokesh
+//// Version     :
+//// Copyright   : Your copyright notice
+//// Description : Hello World in C++, Ansi-style
+////============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "1234";
+	return 0;
+}
+
+//original copy
